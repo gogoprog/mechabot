3cb24a2e71a1427abbe5d97e1004ccc5e74e5a7f
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-08-25 13:54:02 +0200

    up

--- src/main.lua
+++ src/main.lua
@@ -3,10 +3,9 @@ require 'game'
 function init()
     gengine.application.setName("Mechabot")
     gengine.application.setExtent(960, 540)
+    --gengine.application.setExtent(1920, 1080)
     gengine.application.setFullscreen(false)
 end
-
-local logoEntity
 
 function start()
     gengine.graphics.setClearColor(0.0,0.0,0.1,1)
