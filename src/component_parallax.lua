fbfe43e947532a1503280dca7e6c09cb78f78eb6
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-30 14:19:51 +0200

    Fixed parallaxes

--- src/component_parallax.lua
+++ src/component_parallax.lua
@@ -8,8 +8,8 @@ end
 
 function ComponentParallax:update(dt)
     local e = self.entity
-
-    e.sprite.uvOffset = Vector2(Map.parallaxOffset * self.speed, 0)
+    local offset = Map.parallaxOffset * self.speed
+    e.sprite.textureRect = Rect(Vector2(offset, 1), Vector2(1 + offset, 0))
 end
 
 function ComponentParallax:remove()
